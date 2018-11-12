737b0f8d50d4c09c09e54009c301db624ff03005
Dan Mosora <dan@stitchdata.com>
2018-11-12 20:44:09 +0000

    Version 0.4.4, bump requests to 2.20.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-wootric',
-      version='0.4.3',
+      version='0.4.4',
       description='Singer.io tap for extracting data from the Wootric API',
       author='Stitch',
       url='http://singer.io',
@@ -11,7 +11,7 @@ setup(name='tap-wootric',
       py_modules=['tap_CHANGEME'],
       install_requires=[
           'singer-python==1.2.0',
-          'requests==2.12.4',
+          'requests==2.20.0',
           'backoff==1.3.2'
       ],
       entry_points='''
